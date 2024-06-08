4526572196392d9a9e02f5092692da7fd004ffc5
Mark Clifford <markclifford@Marks-MBP.attlocal.net>
2024-06-07 21:14:12 -0700

    invoice layout updates

--- scripts/invoice_generator.py
+++ scripts/invoice_generator.py
@@ -1,41 +1,51 @@
-import pandas as pd     
+import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 import os
 
-spreadsheet_path = '../data/spreadsheet.xlsx'  # Update this path if necessary
+spreadsheet_path = './data/lesson_tracker.xlsx'  # Update this path if necessary
 data = pd.read_excel(spreadsheet_path)
 
 # Group data by household
 grouped_data = data.groupby('Household')
 
 # Set up Jinja2 template environment
-env = Environment(loader=FileSystemLoader('../templates'))
+env = Environment(loader=FileSystemLoader('./templates'))
 template = env.get_template('invoice_template.html')
 
 # Function to generate invoice PDFs
 def generate_invoice(household_name, household_data):
     lessons = household_data.to_dict(orient='records')
-    total_amount_due = household_data['Amount Due'].apply(lambda x: float(x.replace('$', ''))).sum()
+    total_amount_due = household_data['Amount Due'].apply(lambda x: float(str(x).replace('$', ''))).sum()
+    total_amount_due_formatted = "${:,.2f}".format(total_amount_due)
     invoice_number = household_data['Invoice No.'].values[0]
+    household_fullname = household_data['household_fullname1'].values[0]
+    household_email = household_data['household_email1'].values[0]
     
-    invoice_html = template.render(
-        invoice_number=invoice_number,
-        date=pd.to_datetime('today').strftime('%Y-%m-%d'),
-        teacher_name="Your Name",
-        teacher_address="Your Address",
-        teacher_email="Your Email",
-        household_name=household_name,
-        lessons=lessons,
-        total_amount_due=total_amount_due
-    )
+    context = {
+        'invoice_number': invoice_number,
+        'date': pd.to_datetime('today').strftime('%Y-%m-%d'),
+        'teacher_name': "Mark Clifford",
+        'teacher_address': "3007 Peralta St",
+        'teacher_city': "Oakland, CA 94618",
+        'teacher_phone': "415.368.1448",
+        'teacher_email': "markmclifford@gmail.com",
+        'household_fullname': household_fullname,
+        'household_email': household_email,
+        'household_name': household_name,
+        'lessons': lessons,
+        'total_amount_due': total_amount_due_formatted
+    }
+    print(context)  # Print context to verify
+
+    invoice_html = template.render(**context)
     
     html = HTML(string=invoice_html)
-    pdf_path = f"../invoices/invoice_{household_name}_{invoice_number}.pdf"
+    pdf_path = f"./invoices/invoice_{household_name}_{invoice_number}.pdf"
     html.write_pdf(pdf_path)
 
 # Create output directory if it doesn't exist
-os.makedirs('../invoices', exist_ok=True)
+os.makedirs('./invoices', exist_ok=True)
 
 # Generate invoices for all households
 for household_name, household_data in grouped_data:
